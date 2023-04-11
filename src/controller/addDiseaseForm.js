c6bb62403e147e073a11eb24d7aef9d2b7392132
Hafsa <hafsa390@gmail.com>
2023-04-11 06:53:23 -0400

    add, edit and delete functionality is restricted

--- src/controller/addDiseaseForm.js
+++ src/controller/addDiseaseForm.js
@@ -26,20 +26,20 @@ document
   });
 
 document.getElementById("submitBtn").addEventListener("click", () => {
-  let files = undefined;
-  let refs = undefined;
-  if (fileList !== undefined && fileList.length > 0) {
-    files = [];
-    refs = [];
-    Object.keys(fileList).forEach((key) => {
-      const file = fileList[key];
-      const ref = document.getElementById(`input-${file.name}`).value;
-      files.push(file);
-      refs.push(ref);
-    });
-  }
-  addDisease(files, refs);
-  // alert("You are not allowed to add new disease record.")  
+  // let files = undefined;
+  // let refs = undefined;
+  // if (fileList !== undefined && fileList.length > 0) {
+  //   files = [];
+  //   refs = [];
+  //   Object.keys(fileList).forEach((key) => {
+  //     const file = fileList[key];
+  //     const ref = document.getElementById(`input-${file.name}`).value;
+  //     files.push(file);
+  //     refs.push(ref);
+  //   });
+  // }
+  // addDisease(files, refs);
+  alert("You are not allowed to add new disease record.")  
 });
 
 document.getElementById("cancelBtnId").addEventListener("click", function () {
